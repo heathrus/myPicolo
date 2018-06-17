5f7f9994f2bc0b10c45474bd6a4bb941b168957b
Heath Rusby <ruzzberry0@gmail.com>
2018-06-18 02:10:39 +0930

    Initial Commit

---
+++ myPicolo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myPicolo
+//
+//  Created by Heath Rusby on 18/6/18.
+//  Copyright © 2018 Heath Rusby. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
